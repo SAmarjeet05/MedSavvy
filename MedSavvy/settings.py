65d7cd3d03f0296c6fb983097c700cb86a9bb298
SAmarjeet05 <amar2005.reet@gmail.com>
2025-08-24 02:38:34 +0530

    Update settings.py

--- MedSavvy/settings.py
+++ MedSavvy/settings.py
@@ -142,24 +142,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
-# Where collectstatic will put all static files for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Additional locations of static files (your project-level static dir)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-# Media files (user-uploaded content)
 MEDIA_URL = '/images/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-# WhiteNoise storage (for Render)
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ### SSLCOMMERZ env variables
 #VARIABLE should be in capital letter.
